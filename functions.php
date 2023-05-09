67bbf29d63b3a1b1e8841a5eaeeee675f383e020
Bjarne666 <bjarne96@outlook.dk>
2023-05-09 15:02:41 +0200

    removed search for now

--- functions.php
+++ functions.php
@@ -1,12 +1,4 @@
-<body>
-    <div class="header">
-        <div class="container">
-            <?php the_custom_logo() ?>
-            <h1><?php bloginfo("name") ?></h1>
-            <p><?php bloginfo("description") ?></p>
-            <?php get_search_form() ?>
-        </div>
-    </div>
+
     <?php
     // Exit if accessed directly
 
--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php get_header() ?>
 
 <body>
-    <header>
-        <div class="logo"><a href="<?php echo site_url() ?>"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/header.png" alt="logo" style="width: auto; height: auto;"></a></div>
-        <?php wp_nav_menu(array("theme_location" => "header-menu-location")) ?>
-    </header>
+
     <?php while (have_posts()) : the_post() ?>
-        <div class="main">
-            <div class="forside1 row">
-                <div class="col-12 col-sm-6 "><?php the_field("forside_overskrift") ?></div>
-                <div class="col-12 col-sm-6 "><?php the_field("forside_brodtekst") ?></div>
-            </div>
+        <div class="forside row">
+            <div class="col-12 col-sm-6 "><?php the_field("forside_overskrift") ?></div>
+            <div class="col-12 col-sm-6 "><?php the_field("forside_brodtekst") ?></div>
         </div>
 
     <?php endwhile ?>
